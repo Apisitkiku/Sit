09ceaa72163e5ace6a5e6b7bfd6c966f2744e772
Sitkiku Love <Sitkiku Love>
2017-04-10 10:35:23 +0700

    Test

---
+++ src/Sit2/Mean.java
@@ -0,0 +1,12 @@
+package Sit2;
+
+public class Mean {
+
+	public static void main(String[] args) {
+		int a=1;
+		for(a=1;a<7;a++);
+		a=a+a;
+		System.out.println(a);
+	}
+	
+}
---
+++ src/Sit2/Med.java
@@ -0,0 +1,5 @@
+package Sit2;
+
+public class Med {
+
+}
